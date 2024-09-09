e105a469a49da0c64a8253a2d31c5efef101acb8
kingola1 <segun8427@gmail.com>
2024-09-09 15:01:30 +0100

    config table

--- database/migrations/2024_08_31_171059_create_configs_table.php
+++ database/migrations/2024_08_31_171059_create_configs_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('key');
             $table->string('value');
             $table->string('name');
-
             $table->timestamps();
         });
     }
